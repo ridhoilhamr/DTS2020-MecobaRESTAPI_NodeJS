4933a03ba79cddf7a89df261f45174d1da5fa87d
ridhoilhamr <ridhoilhamr111@gmail.com>
2020-10-14 17:42:51 +0700

    update & find homework

--- router.js
+++ router.js
@@ -27,6 +27,58 @@ router.post('/homework', async (req,res) => {
     }
 });
 
+//@decs Get all homework
+//@route GET /api/homework
+router.get('/homework', async (req,res) => {
+    const homework =  await Homework.find({});
+
+    if(homework && homework.length !== 0) {
+        res.json(homework)
+    } else {
+        res.status(404).json({
+            message: 'Homework not found'
+        });
+    }
+});
+
+//@decs Get a homework
+//@route GET /api/homework/:id
+router.get('/homework/:id', async (req,res) => {
+    const homework = await Homework.findById(req.params.id);
+
+if(homework) {
+    res.json(homework)
+    } else {
+        res.status(404).json({
+            message: 'Homework not found'
+        });
+    }
+});
+
+//@decs Update a homework
+//@route PUT /api/homework/:id
+//edit data in server
+router.put('/homework/:id', async (req,res) => {
+    const {course, title, due_date, status} = req.body;
+
+    const homework = await Homework.findById(req.params.id);
+
+    if (homework) {
+        homework.course = course;
+        homework.title = title;
+        homework.due_date = due_date;
+        homework.status = status;
+
+        const updateHomewok = await homework.save()
+
+        res.send(updateHomewok);
+    } else {
+        res.status(404).json({
+            message: 'Homework not found'
+        })
+    }
+})
+
 
 
 export default router;
